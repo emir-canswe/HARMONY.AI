55711c6e71cb6d03008d1566a589c9277a57d2ab
emir-canswe <canemircan38@gmail.com>
2025-07-13 11:48:12 +0300

    Update HARMONY.AI.py

--- HARMONY.AI.py
+++ HARMONY.AI.py
@@ -1,18 +1,18 @@
-import cv2 as cv
+import cv2 as cv#open cv kur
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(0)#kameraya baglan
 
-while True:
-    ret, frame = cap.read()
+while True:#esc tusuna basaa-na kadar sonsuz dongu olustır
+    ret, frame = cap.read()#kerayi oku
     if not ret:
         break
 
     frame = cv.flip(frame, 1)  # Görüntüyü ters çevir (ayna gibi)
 
-    cv.imshow("Aynalanmış Kamera", frame)
+    cv.imshow("Aynalanmış Kamera", frame)#kemrayi goster
 
     if cv.waitKey(1) & 0xFF == 27:  # ESC tuşu ile çık
         break
 
-cap.release()
+cap.release()#kemraya ulasmanini engelle
 cv.destroyAllWindows()
